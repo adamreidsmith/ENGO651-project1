14dcab672d518a5a9bcd01d4a6522f6b09135144
Adam Smith <adamrsmith98@gmail.com>
2023-01-30 19:47:56 -0700

    Created search and book pages

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
@@ -24,3 +25,40 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return "Project 1: TODO"
+
+
+# The search page
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+
+    if request.method == 'GET':
+        return render_template('search.html', results=None)
+    
+    # Get the search input
+    query = request.form.get('query').lower()
+
+    # Modify the query to handle special characters (replace them with any character)
+    query = ''.join(c if c.isalnum() or c == ' ' else '_' for c in query)
+
+    # Form and execute the SQL query
+    sql_query = f"""
+    SELECT * FROM books
+    WHERE LOWER(title) LIKE '%{query}%'
+    OR LOWER(isbn) LIKE '%{query}%'
+    OR LOWER(author) LIKE '%{query}%';
+    """
+    results = list(db.execute(text(sql_query)))
+
+    # Render the results page (same page, but showing new results)
+    return render_template('search.html', results=results)
+
+
+# The book page
+@app.route('/book/<string:isbn>')
+def book(isbn):
+
+    # Get the book corresponding to the isbn
+    book = db.execute(text(f"SELECT * FROM books WHERE isbn = '{isbn}'")).fetchone()
+
+    # Render the book page
+    return render_template('book.html', book=book)
