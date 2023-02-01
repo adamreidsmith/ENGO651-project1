43ee4c2b9e083803169c6eeb5b29594462873d7e
Adam Smith <adamrsmith98@gmail.com>
2023-01-31 17:01:25 -0700

    Update styling and add additional username/password checks

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Project 1
 
-ENGO 551
+ENGO 551 - Adv. Topics on Geospatial Technologies
+
+## Description
+
+Here, we implement a book review website called *Bookshelf*. This website allows registered users to search for books and leave reviews for books.  On the home page, users are prompted to either sign in or sign up for the website.  The webapp is inaccesisble unless users sign up.  Once signed in, users can search for books by title, author, or ISBN, and view further information about each book by clicking the title.  On the book page, users can leave reviews for books and see reviews other users have left.
+
+## File descriptions
+
+<!-- The files [index.html](./index.html), [sport.html](./sport.html), [photos.html](./photos.html), and [contact.html](./contact.html) contain the HTML code for the home, sport, photo gallery, and contact pages respectively.  Styling for all the pages is included in the SASS file [style.scss](./style.scss), which SASS converts into [style.css](./style.css).  This file is divided into 6 sections: styling which applies to all pages, styling which applies to each of the four pages, and a media query for adjusting the styling on smaller screens.  The [media](./media) directory contains all images and icons that are displayed on the webpage. -->
+
+## Project requirements
+
+<!-- This project satisfies all requirements:
+* The website contains four pages, and each page is accessable from each other page via the navigation bar in the top left.
+* The [contact.html](./contact.html) page uses an unordered list to display my contact information, a table is used to display recent race results in the [sport.html](./sport.html) page, and several images are dispalyed throughout the site particularly on the [photos.html](./photos.html) page.
+* The styling is contained in [style.scss](./style.scss), which is converted into [style.css](./style.css) by Sass.
+* Many CSS preoperties and selectors are utilized in [style.scss](./style.scss). Some examples of types of selectors I used are:
+    * element1, element2
+    * element1 element2
+    * .class
+    * #id
+    * element1 > element2
+    * element:hover
+* A media query is used such that when the display width reduces below 780 pixels, changes are made to the styling of the navigation bar, the photo carousel, the sport paragraph and results table, and other items.
+* A Bootstrap 4 carousel was used to create a photo gallery on the [photos.html](./photos.html) page.  Bootstrap's grid feature was used to position the contents of the [sport.html](./sport.html) page into two columns when the screen is larger than 780 pixels in width.
+* Two SASS variables are defined at the top of the [style.scss](./style.scss) file.  SASS inheritance was used to change the background image on the header of each page while keeping the styling consistent.  SASS nesting was used to style the table of race results on the [sport.html](./sport.html) page. -->
--- application.py
+++ application.py
@@ -22,73 +22,95 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+# The home page
 @app.route("/")
 def index():
+    if session.get('current_user') is not None:
+        session.pop('current_user')
     return render_template("main.html")
 
 
+# The sign in page 
 @app.route("/signin", methods=["GET"])
 def sign_in():
+    if session.get('current_user') is not None:
+        session.pop('current_user')
     return render_template("sign_in.html")
 
 
+# The sign-up page
 @app.route("/signup", methods=["GET"])
 def sign_up():
+    if session.get('current_user') is not None:
+        session.pop('current_user')
     return render_template("sign_up.html")
 
 
+# Handle the sign-in logic
 @app.route("/login", methods=["POST"])
 def log_in():
-    #get text from input
+    # Get text from input
     username = request.form.get("username")
     password = request.form.get("password")
-    password2 = request.form.get("password2")
+    # password2 = request.form.get("password2")
 
-    #case1: check whether username exists
+    # Case1: check whether username exists
     username_check = db.execute(text(f"SELECT * FROM users WHERE username='{username}'")).fetchall()
 
     if len(username_check) == 0:
-        #button should link to sign-up page *
-        return render_template('error.html', message= "No this user.", button="Sign Up", url='signup')
+        # Button should link to sign-up page *
+        return render_template('error.html', message="Invalid username.", button="Sign Up", url='signup')
     
-    #case2: wrong password
+    # Case2: wrong password
     password_check = db.execute(text(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'")).fetchall()
     if len(password_check) == 0:
-        #button should link to sign-in page *
-        return render_template('error.html', message="Wrong Password.", button="Sign In Again", url='signin')
+        # Button should link to sign-in page *
+        return render_template('error.html', message="Incorrect password.", button="Sign In Again", url='signin')
 
-    #store current user
-    if request.method=="POST":
-        session["current_user"] = username
+    # Store current user
+    # if request.method == "POST":
+    session["current_user"] = username
     
     return render_template('success.html')
 
 
+# Handle the sign-up logic
 @app.route("/login_new", methods=["POST"])
 def log_in_new():
-    #get text from input
+    # Get text from input
     username = request.form.get("username")
     password = request.form.get("password")
     password2 = request.form.get("password2")
 
-    #case1: check whether username exists
+    # Case1: check whether data has been entered
+    if username == '' or password == '':
+        # Button should link to sign-up page *
+        return render_template('error.html', message='Username or password cannot be empty.', button="Sign Up Again", url='signup')
+
+    # Case2: check whether data has been entered
+    invalid_chars = {"'", '"', ' '}
+    if len(set(username) & invalid_chars) > 0 or len(set(password) & invalid_chars):
+        # Button should link to sign-up page *
+        return render_template('error.html', message='Username or password cannot contain spaces or quote characters.', button="Sign Up Again", url='signup')
+
+    # Case3: check whether username exists
     username_check = db.execute(text(f"SELECT * FROM users WHERE username='{username}'")).fetchall()
     if len(username_check) != 0:
-        #button should link to sign-up page *
-        return render_template('error.html', message= 'Repeated username.', button="Sign Up Again", url='signup')
+        # Button should link to sign-up page *
+        return render_template('error.html', message='Username already exists.', button="Sign Up Again", url='signup')
     
-    #case2: not matched password
+    # Case4: not matched password
     if password != password2:
-        #button should link to sign-in page *
-        return render_template('error.html', message="Password are not matched.", button="Sign Up Again", url='signup')
+        # Button should link to sign-in page *
+        return render_template('error.html', message="Passwords do not match.", button="Sign Up Again", url='signup')
     
     db.execute(text('INSERT INTO users (username, password) VALUES (:username, :password)'),
                 {'username': username, 'password': password})
     db.commit()
 
-    #store current user
-    if request.method=="POST":
-        session["current_user"] = username
+    # Store current user
+    # if request.method=="POST":
+    session["current_user"] = username
     
     return render_template('success.html')
 
@@ -97,16 +119,18 @@ def log_in_new():
 @app.route('/search', methods=['POST', 'GET'])
 def search():
 
+    # Link to sign-in page if no user is currently signed in
     if session.get('current_user') is None:
         return render_template('main.html')
 
+    # Render the search page if no search has happened yet
     if request.method == 'GET':
         return render_template('search.html', results=None)
     
     # Get the search input
     query = request.form.get('query').lower()
 
-    # Modify the query to handle special characters (replace them with any character)
+    # Handle special characters in the query to prevent SQL injections
     query = ''.join(c if c.isalnum() or c == ' ' else '_' for c in query)
 
     # Form and execute the SQL query
@@ -126,21 +150,23 @@ def search():
 @app.route('/book/<string:isbn>', methods=['GET', 'POST'])
 def book(isbn):
 
+    # Link to sign-in page if no user is currently signed in
     if session.get('current_user') is None:
         return render_template('main.html')
 
     if request.method == 'POST':
         # If a post request is sent, add the posted review to the reviews table in the database
-        review = request.form.get('review')
+        review = request.form.get('review').strip()
         if review != '':
             db.execute(text('INSERT INTO reviews (review, username, book_isbn) VALUES (:review, :user, :isbn)'),
                 {'review': review, 'user': session['current_user'], 'isbn': isbn})
+        db.commit()
 
-    # Get the book corresponding to the isbn
+    # Get the book corresponding to the passed in isbn
     book = db.execute(text(f"SELECT * FROM books WHERE isbn = '{isbn}';")).fetchone()
-
     # Get all current review information for the book
     reviews = db.execute(text(f"SELECT review, username FROM reviews WHERE reviews.book_isbn = '{isbn}';")).fetchall()
+    db.commit()
 
     # Render the book page
     return render_template('book.html', book=book, reviews=reviews)
@@ -148,5 +174,7 @@ def book(isbn):
 
 @app.route('/logout')
 def logout():
-    session.pop('current_user')
+    # Log the user out and return to to the homepage
+    if session.get('current_user') is not None:
+        session.pop('current_user')
     return render_template('main.html')
